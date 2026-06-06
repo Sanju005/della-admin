51089aeb70234528e8fe5cdf54f2ae16ccce1b01
Sanju <mrsanju005@gmail.com>
2026-06-06 13:59:10 +0800

    sde

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1780723626996,
+    __BUILD_TIMESTAMP_MS__: 1780725351855,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
